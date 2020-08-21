41cdf5e667d341e45d73e2f2af566350dcb57f65
haidlir <haidlir@gmail.com>
2020-08-21 21:39:38 +0700

    Add more guide and predefined configuration

--- README.md
+++ README.md
@@ -16,6 +16,13 @@ $ docker build -f GenieACS.dockerfile . -t drumsergio/genieacs:local
 $ ./up.sh
 ```
 
+# Pre-Steps
+1. From ACS web, configure CPE to ACS authentication refering to [this guide](https://github.com/genieacs/genieacs/wiki/GenieACS-Auth-Config#cpe-to-acs-in-version-120).
+2. From CPE console, adjust the CPE's configuration.
+3. Overwrite the default provision-script using the [./provision-scripts/Push-Statistics-to-Telegraf-Webhook-Listener.js](https://github.com/haidlir/ACS-Stack/blob/master/provision-scripts/Push-Statistics-to-Telegraf-Webhook-Listener.js) file
+3. [Restart](https://docs.docker.com/engine/reference/commandline/restart/) GenieACS service 
+4. [Import](https://grafana.com/docs/grafana/latest/reference/export_import/#importing-a-dashboard) [grafana/sample-dashboard.json](https://github.com/haidlir/ACS-Stack/blob/master/grafana/sample-dashboard.json) to Grafana
+
 # Sample Result
 - CPE Statistics
 ![CPE Statistics](https://raw.githubusercontent.com/haidlir/ACS-Stack/master/images/cpe-statistics.png "CPE Statistics")
--- provision-scripts/Push-Statistics-to-Telegraf-Webhook-Listener.js
+++ provision-scripts/Push-Statistics-to-Telegraf-Webhook-Listener.js
@@ -13,31 +13,21 @@ declare("InternetGatewayDevice.LANDevice.*.Hosts.Host.*.Active", {path: minutely
 declare("Device.Ethernet.Interface.*.Stats.BytesSent", {path: minutely, value: minutely});
 declare("Device.Ethernet.Interface.*.Stats.BytesReceived", {path: minutely, value: minutely});
 
+log("start logic....");
 let timestamp = Date.now();
 let serialNumber = declare("DeviceID.SerialNumber", {value: 1}).value[0];
-let bytesSents = declare("Device.Ethernet.Interface.*.Stats.BytesSent", {value: 1});
-let bytesReceiveds = declare("Device.Ethernet.Interface.*.Stats.BytesReceived", {value: 1});
+
+let params = [
+  {"measurement": "CPEByteStats", "path": "Device.Ethernet.Interface.*.Stats.BytesSent"},
+  {"measurement": "CPEByteStats", "path": "Device.Ethernet.Interface.*.Stats.BytesReceived"},
+  {"measurement": "CPEDescription", "path": "DeviceID.Manufacturer"},
+  {"measurement": "CPEDescription", "path": "InternetGatewayDevice.DeviceInfo.HardwareVersion"},
+  {"measurement": "CPEWifiStats", "path": "  InternetGatewayDevice.LANDevice.*.WLANConfiguration.*.SSID"},
+];
+
 let points = []
-
-
-if (bytesSents.size) {
-  for (let p of bytesSents) {
-    let point = {};
-    point["serialNumber"] = serialNumber;
-    point[p.path] = p.value[0];
-    point["timestamp"] = timestamp;
-    points.push(point);
-  }
-}
-
-if (bytesReceiveds.size) {
-  for (let p of bytesReceiveds) {
-    let point = {};
-    point["serialNumber"] = serialNumber;
-    point[p.path] = p.value[0];
-    point["timestamp"] = timestamp;
-    points.push(point);
-  }
+for (let param of params) {
+  points = appendPointFromParam(points, param["measurement"], param["path"])
 }
 
 let activeHost = declare("InternetGatewayDevice.LANDevice.*.Hosts.Host.*.Active", {value: 1});
@@ -52,8 +42,25 @@ if (activeHost.size) {
   point["serialNumber"] = serialNumber;
   point["activeHost"] = numActiveHost;
   point["timestamp"] = timestamp;
+  point["measurement"] = "CPEWifiStats";
   points.push(point);
 }
 
 let url = "http://telegraf:9111/CPEStats";
-ext('ext-sample', 'PostWebhook', url, JSON.stringify(points));
+let result = ext('ext-sample', 'PostWebhook', url, JSON.stringify(points));
+log("finish");
+
+function appendPointFromParam(points, measurement, path) {
+  let resp = declare(path, {value: 1});
+  if (resp.size) {
+    for (let p of resp) {
+      let point = {};
+      point["measurement"] = measurement;
+      point["serialNumber"] = serialNumber;
+      point[p.path] = p.value[0];
+      point["timestamp"] = timestamp;
+      points.push(point);
+    }
+  }
+  return points
+}
